e714bb29d5307a6d4aa300218a5eadab2fd0d2d8
William Svoboda <wsvoboda@princeton.edu>
2021-03-09 09:30:34 -0500

    Add basic node representation, mouse handling

--- visualizer.py
+++ visualizer.py
@@ -1,24 +1,70 @@
 import sys
 import pygame
+from pygame.constants import KMOD_CTRL
+from pygame.cursors import tri_right
 
-# Constants
+# Game settings
 SIZE = WIDTH, HEIGHT = 800, 800
 TITLE = 'Visualizer'
+ROWS = 8
+COLS = 8
+w = WIDTH / COLS
+h = HEIGHT / ROWS
+# Constants
+WHITE = pygame.Color(255, 255, 255)
+GREY = pygame.Color(128, 128, 128)
+BLACK = pygame.Color(0, 0, 0)
+
+# A single vertex in the graph
+class Node:
+    def __init__(self, x, y) -> None:
+        # Position on screen
+        self.x = x
+        self.y = y
+        # Neighboring vertices
+        self.neighbors = []
+        # Is blocked
+        self.blocked = False
+    def draw(self):
+        color = BLACK if self.blocked else WHITE
+        pygame.draw.rect(screen, color, (self.x * w, self.y * h, w, h))
+        pygame.display.update()
+
+# Handle mouse click
+def handleMouseClick(x, block):
+    t = x[0]
+    w = x[1]
+    g1 = t // (WIDTH // COLS)
+    g2 = w // (HEIGHT // ROWS)
+    node = graph[g1][g2]
+    node.blocked = True if block else False
+    node.draw()
 
 # Initialize game
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption(TITLE)
 
+# Create nodes
+graph = [[0 for c in range(COLS)] for r in range(ROWS)]
+for i in range(COLS):
+        for j in range(ROWS):
+            graph[i][j] = Node(i, j)
+            graph[i][j].draw()
+
 # Main game loop
 while True:
     # Handle events
     events = pygame.event.get()
     for event in events:
+        mods = pygame.key.get_mods()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    # Draw red background on screen
-    screen.fill(pygame.Color(255,0,0))
-    pygame.display.update()
+        elif (pygame.mouse.get_pressed()[1] or mods & 
+        pygame.KMOD_CTRL and pygame.mouse.get_pressed()[0]):
+            mousePosition = pygame.mouse.get_pos()
+            handleMouseClick(mousePosition, False)
+        elif pygame.mouse.get_pressed()[0]:
+            mousePosition = pygame.mouse.get_pos()
+            handleMouseClick(mousePosition, True)
