80df54df42e1773140fc75228814c95ca1e96026
William Svoboda <wsvoboda@princeton.edu>
2021-03-23 22:04:39 -0400

    Add reference bfs solution, visualization of final path

--- visualizer.py
+++ visualizer.py
@@ -21,11 +21,13 @@ GREY = pygame.Color(128, 128, 128)
 BLACK = pygame.Color(0, 0, 0)
 RED = pygame.Color(255, 0, 0)
 BLUE = pygame.Color(0, 0, 255)
+GREEN = pygame.Color(0, 255, 0)
 
 # All possible gamestates
 class GameState(Enum):
     INPUT = auto()
     SEARCH = auto()
+    END = auto()
 
 # A single vertex in the graph
 class Node:
@@ -34,7 +36,7 @@ class Node:
         UNBLOCKED = auto()
         BLOCKED = auto()
         START = auto()
-        END = auto()
+        GOAL = auto()
     # Create a new node
     def __init__(self, x, y) -> None:
         # Position on screen
@@ -51,7 +53,7 @@ class Node:
             color = BLACK
         elif self.type == self.NodeType.START:
             color = RED
-        elif self.type == self.NodeType.END:
+        elif self.type == self.NodeType.GOAL:
             color = BLUE
         pygame.draw.rect(screen, color, (self.x * w + BORDER, self.y * h + BORDER, w - BORDER, h - BORDER))
         pygame.display.update()
@@ -59,13 +61,13 @@ class Node:
     def addNeighbors(self, graph):
         x = self.x
         y = self.y
-        if x < COLS-1 and graph[self.x + 1][y].type == Node.NodeType.UNBLOCKED:
+        if x < COLS-1 and graph[self.x + 1][y].type != Node.NodeType.BLOCKED:
             self.neighbors.append(graph[self.x + 1][y])
-        if x > 0 and graph[self.x - 1][y].type == Node.NodeType.UNBLOCKED:
+        if x > 0 and graph[self.x - 1][y].type != Node.NodeType.BLOCKED:
             self.neighbors.append(graph[self.x - 1][y])
-        if y < ROWS-1 and graph[self.x][y + 1].type == Node.NodeType.UNBLOCKED:
+        if y < ROWS-1 and graph[self.x][y + 1].type != Node.NodeType.BLOCKED:
             self.neighbors.append(graph[self.x][y + 1])
-        if y > 0 and graph[self.x][y - 1].type == Node.NodeType.UNBLOCKED:
+        if y > 0 and graph[self.x][y - 1].type != Node.NodeType.BLOCKED:
             self.neighbors.append(graph[self.x][y - 1])
 
 # Interact with selected node
@@ -75,9 +77,30 @@ def handleMouseClick(x, type, graph):
     g1 = t // (WIDTH // COLS)
     g2 = w // (HEIGHT // ROWS)
     node = graph[g1][g2]
-    if node.type != node.NodeType.START and node.type != node.NodeType.END:
+    if node.type != node.NodeType.START and node.type != node.NodeType.GOAL:
         node.type = type
         node.draw()
+
+# Breadth-first search to find the shortest path between two nodes
+# Adapted from a blog post by Valerio Valardo on 03/18/2017
+# https://pythoninwonderland.wordpress.com/2017/03/18/how-to-implement-breadth-first-search-in-python/
+def bfs_shortest_path(start, goal):
+    explored = []
+    queue = [[start]]
+    if start == goal:
+        raise RuntimeError('Start and Goal nodes cannot be the same!')
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in explored:
+            for neighbor in node.neighbors:
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+                if neighbor == goal:
+                    return new_path
+            explored.append(node)
+    raise RuntimeError('No path exists between start and goal nodes!')
 
 # Initialize game
 pygame.init()
@@ -96,9 +119,9 @@ for i in range(COLS):
 start = graph[0][COLS - 1]
 start.type = Node.NodeType.START
 start.draw()
-end = graph[ROWS - 1][0]
-end.type = Node.NodeType.END
-end.draw()
+goal = graph[ROWS - 1][0]
+goal.type = Node.NodeType.GOAL
+goal.draw()
 
 # Main game loop
 current_state = GameState.INPUT
@@ -125,8 +148,6 @@ while True:
                 for i in range(COLS):
                     for j in range(ROWS):
                         graph[i][j].addNeighbors(graph)
-                print(start.neighbors)
-                print(end.neighbors)
                 current_state = GameState.SEARCH
     elif current_state == GameState.SEARCH: # Visualize graph traversal
         for event in events:
@@ -134,3 +155,18 @@ while True:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            else:
+                path = bfs_shortest_path(start, goal)
+                for node in path:
+                    if node != start and node != goal:
+                        pygame.draw.rect(screen, GREEN, (node.x * w + BORDER, node.y * h + BORDER, w - BORDER, h - BORDER))
+                        pygame.display.update()
+                current_state = GameState.END
+    elif current_state == GameState.END: # End the game
+        for event in events:
+            # Check if user quits the game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            else:
+                pass
